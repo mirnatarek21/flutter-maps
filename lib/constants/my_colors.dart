85e25cde426bde8b096df591f30c8bba40de2d7f
mirnatarek21 <mirnatarek60@gmail.com>
2025-03-02 17:23:50 +0200

    add helper files

---
+++ lib/constants/my_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color green = Color(0xff1CAC78);
+  static const Color green2 = Color(0xff17B169);
+  static const Color blue = Color(0xff4B9CD3);
+  static const Color blueGrey = Color(0xff6699CC);
+  static const Color lightGrey = Color(0xffD3D3D3);
+}
